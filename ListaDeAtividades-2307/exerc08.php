b3d210bf1ee0a177a6ceeab440bfb6dc6a100f70
Rh4yanna <rhayannaborges6@gmail.com>
2026-08-13 19:40:11 -0300

    Corrigindo memes

--- ListaDeAtividades-2307/exerc08.php
+++ ListaDeAtividades-2307/exerc08.php
@@ -1,5 +1,11 @@
 <?php
-$memes = ["Doggo", "Gigachad", "Pepe the Frog", "Distracted Boyfriend", "Think About It"];
+$memes = [
+    "Grávida de Taubaté",
+    "Nazaré Confusa",
+    "Caneta Azul",
+    "Bora Bill",
+    "Casimiro (Meteu essa?)"
+];
 
 echo "<h2>Lista de Memes</h2>";
 foreach ($memes as $meme) {
