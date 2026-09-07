52e1293ec6a5fafc5d28369edcb758632150e817
Rh4yanna <rhayannaborges6@gmail.com>
2026-09-07 17:01:28 -0300

    ATV 18: instalar Breeze com login, registro e recuperacao de senha

--- ListaDeAtividades-Laravel/routes/web.php
+++ ListaDeAtividades-Laravel/routes/web.php
@@ -4,11 +4,19 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', fn () => view('home'));
 Route::get('/sobre', fn () => 'Sobre o projeto Laravel');
-Route::resource('alunos', \App\Http\Controllers\AlunoController::class);
+Route::resource('alunos', \App\Http\Controllers\AlunoController::class)->middleware('auth');
 Route::get('/contato', fn () => 'Contato da escola');
 
 foreach (['produto', 'categoria', 'usuario'] as $recurso) {
     Route::get('/'.$recurso.'/{id}', fn (string $id) => ucfirst($recurso).': '.$id);
 }
 
-Route::get('/cursos/{curso}', fn (\App\Models\Curso $curso) => view('cursos.show', ['curso' => $curso->load('alunos')]))->name('cursos.show');
+Route::get('/cursos/{curso}', fn (\App\Models\Curso $curso) => view('cursos.show', ['curso' => $curso->load('alunos')]))->middleware('auth')->name('cursos.show');
+
+Route::get('/dashboard', fn () => view('dashboard'))->middleware('auth')->name('dashboard');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [\App\Http\Controllers\ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [\App\Http\Controllers\ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [\App\Http\Controllers\ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+require __DIR__.'/auth.php';
