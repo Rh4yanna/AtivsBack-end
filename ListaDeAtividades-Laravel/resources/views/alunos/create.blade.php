1f719792528a369b3f31f5185e0bf10c7eeeb94b
Rh4yanna <rhayannaborges6@gmail.com>
2026-09-07 16:58:40 -0300

    ATV 14: criar formularios de cadastro e edicao com CSRF

--- ListaDeAtividades-Laravel/resources/views/alunos/create.blade.php
+++ ListaDeAtividades-Laravel/resources/views/alunos/create.blade.php
@@ -1,4 +1,2 @@
 @extends('layouts.app')
-@section('content')
-<h1>Cadastrar aluno</h1>
-@endsection
+@section('content')<h1>Cadastrar aluno</h1><form method="POST" action="{{ route('alunos.store') }}">@include('alunos._form')</form>@endsection
--- ListaDeAtividades-Laravel/resources/views/alunos/edit.blade.php
+++ ListaDeAtividades-Laravel/resources/views/alunos/edit.blade.php
@@ -1,1 +1,2 @@
-<h1>Alunos: edit</h1>
+@extends('layouts.app')
+@section('content')<h1>Editar aluno</h1><form method="POST" action="{{ route('alunos.update', $aluno) }}">@method('PUT') @include('alunos._form')</form>@endsection
