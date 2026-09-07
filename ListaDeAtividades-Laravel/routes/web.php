45e282aa246f8486b854fc9c604e3a91322d02b4
Rh4yanna <rhayannaborges6@gmail.com>
2026-09-07 16:58:38 -0300

    ATV 2: criar rotas com parametros

--- ListaDeAtividades-Laravel/routes/web.php
+++ ListaDeAtividades-Laravel/routes/web.php
@@ -6,3 +6,7 @@ Route::get('/', fn () => view('welcome'));
 Route::get('/sobre', fn () => 'Sobre o projeto Laravel');
 Route::get('/alunos', fn () => 'Lista de alunos');
 Route::get('/contato', fn () => 'Contato da escola');
+
+foreach (['produto', 'categoria', 'usuario'] as $recurso) {
+    Route::get('/'.$recurso.'/{id}', fn (string $id) => ucfirst($recurso).': '.$id);
+}
